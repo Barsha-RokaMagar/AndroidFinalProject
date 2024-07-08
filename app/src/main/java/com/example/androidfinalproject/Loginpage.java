3457513a61bc0c669e8ea9fb63443c5c4be10752
Barsha-RokaMagar <130592450+Barsha-RokaMagar@users.noreply.github.com>
2024-07-08 14:05:46 -0400

    firebase connection

--- app/src/main/java/com/example/androidfinalproject/Loginpage.java
+++ app/src/main/java/com/example/androidfinalproject/Loginpage.java
@@ -65,6 +65,7 @@ public class Loginpage extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 checkuser();
             }
         });
