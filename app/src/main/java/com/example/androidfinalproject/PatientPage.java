42f4793940b41335842f7b3458cbcb8359ce6336
Barsha-RokaMagar <130592450+Barsha-RokaMagar@users.noreply.github.com>
2024-07-16 01:04:24 -0400

    linked patientpage to new page cariologist

--- app/src/main/java/com/example/androidfinalproject/PatientPage.java
+++ app/src/main/java/com/example/androidfinalproject/PatientPage.java
@@ -1,9 +1,14 @@
 package com.example.androidfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class PatientPage extends AppCompatActivity {
 
@@ -12,6 +17,16 @@ public class PatientPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_patient_page);
+
+        LinearLayout cardiologistLayout = findViewById(R.id.cardiologistLayout);
+
+        cardiologistLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientPage.this, CardiologistPage.class);
+                startActivity(intent);
+            }
+        });
 
 
 
