6c299a415ecede1dffb07171eaa0cf969814da0d
Barsha-RokaMagar <130592450+Barsha-RokaMagar@users.noreply.github.com>
2024-07-19 21:47:09 -0400

    added button in patients page

--- app/src/main/java/com/example/androidfinalproject/PatientPage.java
+++ app/src/main/java/com/example/androidfinalproject/PatientPage.java
@@ -7,10 +7,12 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 
 public class PatientPage extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,24 @@ public class PatientPage extends AppCompatActivity {
         setContentView(R.layout.activity_patient_page);
 
         LinearLayout cardiologistLayout = findViewById(R.id.cardiologistLayout);
+        btn = findViewById(R.id.gobackBtn);
+
 
         cardiologistLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PatientPage.this, CardiologistPage.class);
                 startActivity(intent);
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientPage.this, Loginpage.class);
+                startActivity(intent);
+
+
             }
         });
 
